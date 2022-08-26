353f1d4778249a9b05039e3c895efd42f46671a6
Greg Flaherty <gregoryjflaherty@gmail.com>
2022-08-26 09:10:46 -0400

    Create check if player has lost tests

--- test/player-test.js
+++ test/player-test.js
@@ -24,4 +24,23 @@ describe('Player', function () {
         let player = new Player('Clarisa', deck)
         assert.equal(player.name, 'Clarisa')
     })
+    
+    it('has a deck', function (){
+        let player = new Player('Clarisa', deck)
+        assert.equal(player.deck, deck)
+    })
+    
+    it('checks if player has lost', function (){
+        let player = new Player('Clarisa', deck)
+        assert.equal(player.hasLost(), false)
+    })
+    
+    it('returns true if player has no cards left', function (){
+        let player = new Player('Clarisa', deck)
+        for(let i = 0; i < 3; i++){
+            player.deck.removeCard()
+        }
+
+        assert.equal(player.hasLost(), true)
+    })
 })  
